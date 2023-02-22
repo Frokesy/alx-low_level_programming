b63ec602e68d1ab32d4e81c8dbcdaf574c1afe72
Ayanfeoluwa Noah Akindele <98624002+Frokesy@users.noreply.github.com>
2023-02-22 07:12:39 +0100

    fixed bug

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,9 +10,9 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-void times_table(void)
-void print_to_98(int n)
-int add(int a, int b)
+void times_table(void);
+void print_to_98(int n);
+int add(int a, int b);
 
 
 #endif /* MAIN_H */
