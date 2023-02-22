efb4e6f77e1092cc7855fe389e85f0a27440ae60
Ayanfeoluwa Noah Akindele <98624002+Frokesy@users.noreply.github.com>
2023-02-22 06:47:18 +0100

    fix 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,10 +1,12 @@
-#include <stdio.h>
+#include "main.h"
 
 void jack_bauer(void) {
-    int hour, minute;
-    for (hour = 0; hour < 24; hour++) {
-        for (minute = 0; minute < 60; minute++) {
-            printf("%02d:%02d\n", hour, minute);
-        }
-    }
+int hour, minute;
+for (hour = 0; hour < 24; hour++) 
+{
+for (minute = 0; minute < 60; minute++) 
+{
+printf("%02d:%02d\n", hour, minute);
+}
+}
 }
