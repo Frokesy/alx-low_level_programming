252a3cdfb5680bd5c5dcb4a95cb296ec707f2856
Ayanfeoluwa Noah Akindele <98624002+Frokesy@users.noreply.github.com>
2023-02-22 06:51:32 +0100

    more fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,12 +4,12 @@
 int hours, minutes;
 for (hours = 0; hours < 24; hours++) {
 for (minutes = 0; minutes < 60; minutes++) {
-_putchar((hours / 10) + '0');  // print the tens digit of the hour
-_putchar((hours % 10) + '0');  // print the ones digit of the hour
-_putchar(':');  // print the colon separator
-_putchar((minutes / 10) + '0');  // print the tens digit of the minute
-_putchar((minutes % 10) + '0');  // print the ones digit of the minute
-_putchar('\n');  // print a newline character
+_putchar((hours / 10) + '0');
+_putchar((hours % 10) + '0');
+_putchar(':');
+_putchar((minutes / 10) + '0');
+_putchar((minutes % 10) + '0');
+_putchar('\n');
 }
 }
 }
