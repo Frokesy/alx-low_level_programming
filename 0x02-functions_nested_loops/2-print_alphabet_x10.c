0844ed2d2f18c8cfdac9656f8379f13f10bc6961
Ayanfeoluwa Noah Akindele <98624002+Frokesy@users.noreply.github.com>
2023-02-22 06:12:40 +0100

    fix-2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,24 +1,13 @@
-#include <stdio.h>
-/**
- * main - check the code.
- *
- * Return: Always 0.
- */
-int main(void)
+void print_alphabet_x10(void)
 {
-    print_alphabet_x10();
-    return (0);
-}
-
-void print_alphabet_x10(void) {
-    int i = 0;
-    while (i < 10) {
-        char letter = 'a';
-        while (letter <= 'z') {
+    char letter;
+    int i;
+    for (i = 0; i < 10; i++)
+    {
+        for (letter = 'a'; letter <= 'z'; letter++)
+        {
             _putchar(letter);
-            letter++;
         }
-        i++;
+        _putchar('\n');
     }
-    _putchar('\n');
 }
