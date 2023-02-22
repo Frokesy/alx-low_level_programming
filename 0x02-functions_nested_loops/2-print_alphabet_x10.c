c0a50ea20169ad5a7c322f7e1fa68d1e2e95b624
Ayanfeoluwa Noah Akindele <98624002+Frokesy@users.noreply.github.com>
2023-02-22 05:52:53 +0100

    test

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_alphabet_x10();
+    return (0);
+}
 
 void print_alphabet_x10(void) {
     int i = 0;
--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-int main(void) {
-    int test_islower(int c) {
+
+int test_islower(int c) {
     if (c >= 'a' && c <= 'z') {
         return 1;
     } else {
         return 0;
     }
-}
 }
