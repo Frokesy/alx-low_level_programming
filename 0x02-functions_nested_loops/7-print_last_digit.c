c4f846200fa1a12259fdd41802ea529fc1a3f185
Ayanfeoluwa Noah Akindele <98624002+Frokesy@users.noreply.github.com>
2023-02-22 06:38:27 +0100

    fixes

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,10 +1,9 @@
-#include <stdio.h>
+#include "main.h"
 
 int print_last_digit(int n) {
 int last_digit = n % 10;
-if (last_digit < 0) {
+if (last_digit < 0) 
 last_digit = -last_digit;
-}
 printf("%d", last_digit);
 return last_digit;
 }
