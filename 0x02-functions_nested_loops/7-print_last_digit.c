1cecd1de362b112a15ccd502cb7fc635a6a3f1cd
Frokesy <akindeleayanfeoluwa@gmail.com>
2023-02-21 20:26:27 -0800

    mandatory tasks completed

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int print_last_digit(int n) {
+    int last_digit = n % 10;
+    if (last_digit < 0) {
+        last_digit = -last_digit;
+    }
+    printf("%d", last_digit);
+    return last_digit;
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void times_table(void) {
+    int i, j;
+    for (i = 0; i <= 9; i++) {
+        for (j = 0; j <= 9; j++) {
+            printf("%d ", i * j);
+        }
+        printf("\n");
+    }
+}
