70b0c17f3fa05f90337e8677a192609a1772216c
Ayanfeoluwa Noah Akindele <98624002+Frokesy@users.noreply.github.com>
2023-02-22 07:10:58 +0100

    fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,26 +1,16 @@
 #include "main.h"
 
-void times_table(void)
 {
-    int i, j, result;
-
-    for (i = 0; i <= 9; i++) {
-        for (j = 0; j <= 9; j++) {
-            result = i * j;
-            if (j == 0) {
-                _putchar('0');
-            } else if (result < 10) {
-                _putchar(' ');
-                _putchar(result + '0');
-            } else {
-                _putchar(result / 10 + '0');
-                _putchar(result % 10 + '0');
-            }
-            if (j < 9) {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-        _putchar('\n');
-    }
+int hours, minutes;
+for (hours = 0; hours < 24; hours++) {
+for (minutes = 0; minutes < 60; minutes++) {
+_putchar((hours / 10) + '0');
+_putchar((hours % 10) + '0');
+_putchar(':');
+_putchar((minutes / 10) + '0');
+_putchar((minutes % 10) + '0');
+_putchar('\n');
 }
+}
+}
+
