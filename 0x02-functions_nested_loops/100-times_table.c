35ffad1d0fbb475a7db58fc1267b6cb38667f614
Ayanfeoluwa Noah Akindele <98624002+Frokesy@users.noreply.github.com>
2023-02-23 05:04:32 +0100

    last fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,13 +1,13 @@
 include "main.h"
 
-/***
+/**
  * print_times_table - Prints the n times table
  *
  * @n: number times table (0 < n <= 15)
  *
  * Return: no return
  */
-
+ 
 void print_times_table(int n)
 {
 int a, b, op;
@@ -36,7 +36,7 @@ _putchar((op % 10) + 48);
 else
 {
 _putchar(((op / 100) % 10) + 48);
- _putchar(((op / 10) % 10) + 48);
+_putchar(((op / 10) % 10) + 48);
 _putchar((op % 10) + 48);
 }
 }
