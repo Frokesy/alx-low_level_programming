f11f9b0fc0e02ded5411aa5b0a85ca42fcb05f4c
Ayanfeoluwa Noah Akindele <98624002+Frokesy@users.noreply.github.com>
2023-03-03 02:12:40 +0100

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings using at most n bytes of src
+ * @dest: the destination string
+ * @src: the source string
+ * @n: the maximum number of bytes to use from src
+ *
+ * Return: a pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int dest_len = 0, i;
+while (dest[dest_len] != '\0')
+dest_len++;
+for (i = 0; i < n && src[i] != '\0'; i++)
+dest[dest_len + i] = src[i];
+dest[dest_len + i] = '\0';
+return (dest);
+}
