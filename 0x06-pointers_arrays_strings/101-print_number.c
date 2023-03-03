b2b1e3ae92b5a06cafef5b18138b68be245642d5
Ayanfeoluwa Noah Akindele <98624002+Frokesy@users.noreply.github.com>
2023-03-03 03:14:14 +0100

    advanced task 2

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer to print
+ */
+void print_number(int n)
+{
+int divisor = 1;
+if (n < 0) {
+_putchar('-');
+n = -n;
+}
+while (n / divisor > 9) {
+divisor *= 10;
+}
+while (divisor != 0) {
+_putchar('0' + n / divisor);
+n %= divisor;
+divisor /= 10;
+}
+}
--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * cap_string - Capitalizes all words 
- * of a string.
+ * cap_string - Capitalizes all words of a string.
  * @str: The string to be modified.
  *
  * Return: A pointer to the modified string.
