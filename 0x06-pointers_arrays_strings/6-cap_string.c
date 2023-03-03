514c68799b6897cfc18fe91d6a1cd5b8210461e7
Ayanfeoluwa Noah Akindele <98624002+Frokesy@users.noreply.github.com>
2023-03-03 02:48:17 +0100

    mandatory tasks completed

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes all words of a string
+ * @s: the string to capitalize
+ *
+ * Return: pointer to the capitalized string
+ */
+char *cap_string(char *s)
+{
+int i;
+if (s == NULL)
+return (NULL);
+for (i = 0; s[i] != '\0'; i++)
+{
+if (i == 0 || s[i - 1] == ' ' || s[i - 1] == '\t' ||
+s[i - 1] == '\n' || s[i - 1] == ',' || s[i - 1] == ';' ||
+s[i - 1] == '.' || s[i - 1] == '!' || s[i - 1] == '?' ||
+s[i - 1] == '"' || s[i - 1] == '(' || s[i - 1] == ')' ||
+s[i - 1] == '{' || s[i - 1] == '}')
+{
+if (s[i] >= 'a' && s[i] <= 'z')
+{
+s[i] -= 32;
+}
+}
+}
+return (s);
+}
